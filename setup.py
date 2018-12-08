c8e6d10760df41701320680972fff6daaf6bcf77
cryptozeny <cryptozeny@gmail.com>
2018-12-08 20:55:36 +0900

    add personality

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup, Extension
 
-sugar_yespower_module = Extension('sugar_yespower',
+sugarPersonality_yespower_module = Extension('sugarPersonality_yespower',
                             sources = ['yespower.c', 'yespower-opt.c', 'sha256.c'],
                             extra_compile_args=['-march=native', '-funroll-loops', '-fomit-frame-pointer'],
                             include_dirs=['.'])
 
-setup (name = 'sugar_yespower',
+setup (name = 'sugarPersonality_yespower',
        version = '1.0',
-       description = 'Bindings for yespower proof of work used by cryply',
-       ext_modules = [sugar_yespower_module])
+       description = 'Bindings for yespower proof of work used by sugarchain',
+       ext_modules = [sugarPersonality_yespower_module])
--- yespower.c
+++ yespower.c
@@ -37,10 +37,11 @@
 
 static const yespower_params_t yespower_0_5_sugar = {YESPOWER_0_5, 2048, 8, "Client Key", 10};
 static const yespower_params_t yespower_1_0_sugar = {YESPOWER_1_0, 2048, 32, NULL, 0};
+static const yespower_params_t yespower_1_0_sugarPersonality = {YESPOWER_1_0, 2048, 32, "personality test", 16};
 
 int yespower_hash(const uint8_t *input, char *output)
 {
-    if (yespower_tls(input, 80, &yespower_1_0_sugar, (yespower_binary_t *) output))
+    if (yespower_tls(input, 80, &yespower_1_0_sugarPersonality, (yespower_binary_t *) output))
       abort();
 }
 
@@ -66,6 +67,6 @@ static PyMethodDef YespowerMethods[] = {
     { NULL, NULL, 0, NULL }
 };
 
-PyMODINIT_FUNC initsugar_yespower(void) {
-    (void) Py_InitModule("sugar_yespower", YespowerMethods);
+PyMODINIT_FUNC initsugarPersonality_yespower(void) {
+    (void) Py_InitModule("sugarPersonality_yespower", YespowerMethods);
 }
